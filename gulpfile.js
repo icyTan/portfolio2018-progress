4f006e92ceac17484ec6b67a3e3b879c6ed9d265
Unknown <get.patlau@gmail.com>
2018-04-01 15:17:56 -0700

    Basic Dev environment setup continuted

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
+// from https://www.youtube.com/watch?v=0jmNon-R3II
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemap');
 var browsersync = require('browser-sync').create(); // Creates a browser-sync instance
 
 
@@ -8,7 +14,6 @@ var browsersync = require('browser-sync').create(); // Creates a browser-sync in
 // uglify js
 // add modernizr support DONE
 
-// Note this is an example of a higher-order function
 gulp.task('browser-sync', function() {
   browsersync.init({
     server: "app",
@@ -18,8 +23,17 @@ gulp.task('browser-sync', function() {
 
 // can make a default for compile function
 gulp.task('sass', function() {
-  return gulp.src("app/scss/*.scss") // set retrival
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src("app/scss/*.scss") // return this : set retrival
+    .pipe(sourcemaps.init()) // init sourcemaps
+    .pipe(autoprefixer({
+      browser: ['last 2 version'],
+      cascade: false
+    }))
+    .pipe(sass({
+      outputStyle: 'compressed' // output as uglifed
+    })
+    .on('error', sass.logError))
+    .pipe(sourcemaps.write('app/maps')) // write sourcemaps
     .pipe(gulp.dest("app/css")); // set destination
 });
 
@@ -27,7 +41,7 @@ gulp.task('sass', function() {
 // format your gulp watches but then calling on a change to the instance of browsersync PLEASE
 
 gulp.task('watch', ['browser-sync'], function() {
-  gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch('app/scss/*.scss', ['sass']); // watch these items, do this function
   gulp.watch(['app/*.html','app/work/*.html','app/css/*.css','app/js/*.js']).on('change', browsersync.reload); // Should extend this to include css and any other directories
 });
 
