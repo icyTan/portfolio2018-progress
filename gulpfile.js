cc9c67752737805ad81e95da71a4b0600f5987ef
Unknown <get.patlau@gmail.com>
2018-04-01 16:19:42 -0700

    Gulp setup 70%
    TODO:
    setup concat of css files
    setup uglify of js files

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,21 @@ var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
-var sourcemaps = require('gulp-sourcemap');
+var sourcemaps = require('gulp-sourcemaps');
 var browsersync = require('browser-sync').create(); // Creates a browser-sync instance
 
+var jsInput = {
+  js: 'app/js/dev/*.js'
+}
+var jsOutput = 'app/js/dist/'
 
 // TODO:
-// add minification to css
-// uglify js
+// add minification to css TEST
+// uglify js TEST
+// concat all scss
+// eventually pipe a production build
+
+// DONE:
 // add modernizr support DONE
 
 gulp.task('browser-sync', function() {
@@ -21,28 +29,37 @@ gulp.task('browser-sync', function() {
   });
 });
 
-// can make a default for compile function
+// do sass task
 gulp.task('sass', function() {
-  return gulp.src("app/scss/*.scss") // return this : set retrival
-    .pipe(sourcemaps.init()) // init sourcemaps
-    .pipe(autoprefixer({
-      browser: ['last 2 version'],
-      cascade: false
-    }))
+  return gulp.src("app/scss/**/*.scss") // return this : set retrival
+    // .pipe(sourcemaps.init()) // init sourcemaps
     .pipe(sass({
+      errorLogToConsole: true,
       outputStyle: 'compressed' // output as uglifed
     })
     .on('error', sass.logError))
-    .pipe(sourcemaps.write('app/maps')) // write sourcemaps
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    // .pipe(sourcemaps.write('./')) // write sourcemaps
     .pipe(gulp.dest("app/css")); // set destination
+});
+
+gulp.task('js', function(){
+  return gulp.src(jsInput.js)
+  .pipe(concat('app.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('app/js/dist'))
 });
 
 // https://www.browsersync.io/docs/api <--- dont be dumb and follow the new 2.0.0+ API
 // format your gulp watches but then calling on a change to the instance of browsersync PLEASE
 
 gulp.task('watch', ['browser-sync'], function() {
-  gulp.watch('app/scss/*.scss', ['sass']); // watch these items, do this function
-  gulp.watch(['app/*.html','app/work/*.html','app/css/*.css','app/js/*.js']).on('change', browsersync.reload); // Should extend this to include css and any other directories
+  gulp.watch('app/scss/**/*.scss', ['sass']); // watch these items, do this function
+  gulp.watch('app/js/**/*.js',['js']); // watch these items
+  gulp.watch(['app/*.html','app/work/*.html','app/css/*.css','app/js/dev/*.js']).on('change', browsersync.reload); // Should extend this to include css and any other directories
 });
 
 gulp.task('default', ['watch']);
