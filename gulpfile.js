755cb581c6866b3771f56aed8c3a7f17ac4421b8
Unknown <get.patlau@gmail.com>
2018-06-17 19:47:52 -0700

    Lots of changes
    Updated gulp file to have a build distro

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,31 @@ var jsInput = {
 }
 var jsOutput = 'app/js/dist/'
 
+var paths = {
+  src:      'app/src/**/*',
+  srcHTML:  'app/src/**/*.html',
+  srcSCSS:  'app/src/**/*.scss',
+  srcMSCSS: 'app/src/scss/main.scss', // only the main scss file (dont need to grab all the scss partials when processing)
+  srcJS:    'app/src/**/*.js',
+
+  tmp:      'app/tmp',
+  tmpIndex: 'app/tmp/index.html',
+  tmpHTML:  'app/tmp/',
+  tmpCSS:   'app/tmp/css',
+  tmpJS:    'app/tmp/js',
+
+  dist:      'app/dist',
+  distIndex: 'app/dist/index.html',
+  distHTML:  'app/dist/**/*.html',
+  distCSS:   'app/dist/**/*.css',
+  distJS:    'app/dist/**/*.js',
+}
+
+// folder structure
+// src - pre processed
+// tmp - dev server, not minified, procesed
+// dist - processed and minified files
+
 // TODO:
 // uglify js TEST
 // concat all scss
@@ -24,18 +49,24 @@ var jsOutput = 'app/js/dist/'
 
 gulp.task('browser-sync', function() {
   browsersync.init({
-    server: "app",
+    server: "app/tmp",
     port: 5000
   });
 });
 
+// html task
+gulp.task('html', function(){
+  return gulp.src(paths.srcHTML)
+    .pipe(gulp.dest(paths.tmp));
+});
+
 // do sass task
-gulp.task('sass', function() {
-  return gulp.src("app/scss/**/*.scss") // return this : set retrival
+gulp.task('scss', function() {
+  return gulp.src(paths.srcMSCSS) // return this : set retrival
     .pipe(sourcemaps.init()) // init sourcemaps
     .pipe(sass({
       errorLogToConsole: true,
-      outputStyle: 'compressed' // output as uglifed
+      outputStyle: 'expanded' // output as uglifed
     })
     .on('error', sass.logError))
     .pipe(autoprefixer({
@@ -43,23 +74,39 @@ gulp.task('sass', function() {
       cascade: false
     }))
     .pipe(sourcemaps.write('./')) // write sourcemaps
-    .pipe(gulp.dest("app/css")); // set destination
+    .pipe(gulp.dest(paths.tmpCSS)); // set destination
 });
 
 gulp.task('js', function(){
-  return gulp.src(jsInput.js)
+  return gulp.src(paths.srcJS)
   .pipe(concat('app.min.js'))
   // .pipe(uglify()) // TAKE THIS OUT AFTER DEBUG
-  .pipe(gulp.dest('app/js/dist'))
+  .pipe(gulp.dest(paths.tmpJS))
 });
 
 // https://www.browsersync.io/docs/api <--- dont be dumb and follow the new 2.0.0+ API
 // format your gulp watches but then calling on a change to the instance of browsersync PLEASE
 
 gulp.task('watch', ['browser-sync'], function() {
-  gulp.watch('app/scss/**/*.scss', ['sass']); // watch these items, do this function
-  gulp.watch('app/js/**/*.js',['js']); // watch these items
-  gulp.watch(['app/*.html','app/work/*.html','app/css/*.css','app/js/dev/*.js']).on('change', browsersync.reload); // Should extend this to include css and any other directories
+  gulp.watch(paths.srcSCSS, ['sass']); // watch these items, do this function
+  gulp.watch(paths.srcJS,['js']);
+  gulp.watch(paths.src,['refresh']);
+});
+
+gulp.task('refresh', function () {
+  browsersync.reload();
+});
+
+// base copy function
+gulp.task('copyAll', function(){
+  return gulp.src(paths.src)
+    .pipe(gulp.dest(paths.tmp));
+});
+
+// copy and set files for temp server
+gulp.task('copy', ['scss','js'], function(){
+  gulp.src(paths.srcHTML)
+    .pipe(gulp.dest(paths.tmpHTML));
 });
 
 gulp.task('default', ['watch']);
